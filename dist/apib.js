15cf5c109a958b8ad13ae862f916db6a561b49a4
xovel <xovel@vip.qq.com>
2021-06-04 11:27:26 +0800

    v0.2.0
    - drop `axios`.
    - remove `#log` method.
    - remove prefix underline of methods.
    - support keep case for `#type`.
    - drop initialization for options.
    
    KEEP IT BASIC.

--- dist/apib.js
+++ dist/apib.js
@@ -1,39 +1,24 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('axios')) :
-  typeof define === 'function' && define.amd ? define(['axios'], factory) :
-  (global = global || self, global.apib = factory(global.axios));
-}(this, (function (axios) { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = global || self, global.apib = factory());
+}(this, (function () { 'use strict';
 
-  axios = axios && Object.prototype.hasOwnProperty.call(axios, 'default') ? axios['default'] : axios;
-
-  var version = "0.1.0";
+  var version = "0.2.0";
 
   var API = /*#__PURE__*/function () {
     function API() {
-      var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-      this.axios = axios;
       this.version = version;
-      this.options = options;
     }
 
     var _proto = API.prototype;
 
-    _proto.log = function log() {
-      if (this.options.debug) {
-        var _console;
-
-        (_console = console).log.apply(_console, arguments); // eslint-disable-line no-console
-
-      }
-    }
     /**
      * 将对象转为 URL QueryString
      *
      * @param {object} data
      */
-    ;
-
-    _proto._param = function _param(data) {
+    _proto.param = function param(data) {
       // https://jsperf.com/array-push-vs-string-concat
 
       /*
@@ -52,21 +37,22 @@
     /**
      * 类型判断
      *
-     * @param {*} unknown
+     * @param {any} unknown
+     * @param {boolen} keepCase 保持大小写
      * @return {string}
      */
     ;
 
-    _proto._type = function _type(unknown) {
-      var type = Object.prototype.toString.call(unknown); // https://jsperf.com/replace-vs-slice/5
+    _proto.type = function type(unknown, keepCase) {
+      var type = Object.prototype.toString.call(unknown).slice(8, -1); // https://jsperf.com/replace-vs-slice/5
       // return type.replace('[object ', '').replace(']', '').toLowerCase()
 
-      return type.slice(8, -1).toLowerCase();
+      return keepCase ? type : type.toLowerCase();
     }
     /**
      * 将参数列表转为对象形式
      *
-     * @param {array} args 参数原始对象，即 arguments
+     * @param {array} list 参数原始对象，即 arguments
      * @param {array} names 参数名称对应的数组，可以给默认值，默认值需要能被 JSON.parse 解析。
      *
      * 注意：
@@ -74,12 +60,12 @@
      *  - 在遇到对象之后直接退出
      *
      * 示例：
-     *   > _args ([1, 2, {xor: true}], ['offset', 'count'])
+     *   > args([1, 2, {xor: true}], ['offset', 'count'])
      *   < {offset: 1, count: 2, xor: true}
      */
     ;
 
-    _proto._args = function _args(args) {
+    _proto.args = function args(list) {
       var names = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
       var options = {};
       names.forEach(function (item, index) {
@@ -91,23 +77,19 @@
           try {
             options[name] = JSON.parse(temp.slice(1).join('='));
           } catch (e) {// ignore
-          } // const [name, ...value] = item.split('=')
-          // names[index] = name
-          // try {
-          //   options[name] = JSON.parse(value.join('='))
-          // } catch (e) {
-          //   //
-          // }
-
+          }
         }
       });
 
-      for (var i = 0; i < args.length; i++) {
-        if (this._type(args[i]) === 'object') {
-          Object.assign(options, args[i]);
+      for (var i = 0; i < list.length; i++) {
+        var item = list[i];
+        var name = names[i];
+
+        if (this.type(item) === 'object') {
+          Object.assign(options, item);
           break;
-        } else if (this._type(args[i]) !== 'undefined' && this._type(names[i]) !== 'undefined') {
-          options[names[i]] = args[i];
+        } else if (this.type(item) !== 'undefined' && this.type(name) !== 'undefined') {
+          options[name] = item;
         }
       }
 
@@ -120,7 +102,7 @@
      */
     ;
 
-    _proto._join = function _join() {
+    _proto.join = function join() {
       for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
         args[_key] = arguments[_key];
       }
--- test/index.js
+++ test/index.js
@@ -9,7 +9,7 @@ const api = new API()
 describe('args', function () {
   it('base', function () {
     function argsTest() {
-      const options = api._args(arguments, ['offset', 'count'])
+      const options = api.args(arguments, ['offset', 'count'])
       assert.deepStrictEqual(options, {offset: 1, count: 2, xor: true})
     }
     argsTest(1, 2, {xor: true})
@@ -18,12 +18,12 @@ describe('args', function () {
   })
 
   it('default value', function () {
-    assert.deepStrictEqual(api._args(['xovel'], ['name', 'type=1']), {name: 'xovel', type: 1})
+    assert.deepStrictEqual(api.args(['xovel'], ['name', 'type=1']), {name: 'xovel', type: 1})
   })
 
   it('ignore undefined args', function () {
     function argsTest() {
-      const options = api._args(arguments, ['offset', 'count', 'mode'])
+      const options = api.args(arguments, ['offset', 'count', 'mode'])
       assert.deepStrictEqual(options, {offset: 1, mode: 'fire'})
     }
     argsTest(1, ({}).m, 'fire')
@@ -32,7 +32,7 @@ describe('args', function () {
 
   it('ignore undefined names', function () {
     function argsTest() {
-      const options = api._args(arguments, [, 'count', 'mode'])
+      const options = api.args(arguments, [, 'count', 'mode'])
       assert.deepStrictEqual(options, {count: 2, mode: 'fire'})
     }
     argsTest(1, 2, 'fire')
@@ -41,54 +41,59 @@ describe('args', function () {
 
 describe('detect type', function () {
   it('It should return undefined', function () {
-    assert.deepStrictEqual(api._type(), 'undefined')
-    assert.deepStrictEqual(api._type({}.x), 'undefined')
-    assert.deepStrictEqual(api._type(this.y), 'undefined')
+    assert.deepStrictEqual(api.type(), 'undefined')
+    assert.deepStrictEqual(api.type({}.x), 'undefined')
+    assert.deepStrictEqual(api.type(this.y), 'undefined')
   })
 
   it('It should return array', function () {
-    assert.deepStrictEqual(api._type([]), 'array')
+    assert.deepStrictEqual(api.type([]), 'array')
   })
 
   it('It should return object', function () {
-    assert.deepStrictEqual(api._type({}), 'object')
-    assert.deepStrictEqual(api._type(Object.create(null)), 'object')
-    assert.deepStrictEqual(api._type(Object()), 'object')
+    assert.deepStrictEqual(api.type({}), 'object')
+    assert.deepStrictEqual(api.type(Object.create(null)), 'object')
+    assert.deepStrictEqual(api.type(Object()), 'object')
+  })
+
+  it('It should support keep case', function () {
+    assert.deepStrictEqual(api.type(new Date, true), 'Date')
+    assert.deepStrictEqual(api.type(/1/, true), 'RegExp')
   })
 })
 
 describe('param url', function () {
   it('It should return a tring', function () {
-    assert.deepStrictEqual(typeof api._param({}), 'string')
+    assert.deepStrictEqual(typeof api.param({}), 'string')
   })
   it('It could ignore string and number', function () {
-    assert.deepStrictEqual(api._param({name: '6'}), 'name=6')
-    assert.deepStrictEqual(api._param({name: 6}), 'name=6')
+    assert.deepStrictEqual(api.param({name: '6'}), 'name=6')
+    assert.deepStrictEqual(api.param({name: 6}), 'name=6')
   })
   it('It should encodeURIComponent keys and values', function () {
-    assert.deepStrictEqual(api._param({name: '1 2'}), 'name=1%202')
-    assert.deepStrictEqual(api._param({'a,b': 6}), 'a%2Cb=6')
+    assert.deepStrictEqual(api.param({name: '1 2'}), 'name=1%202')
+    assert.deepStrictEqual(api.param({'a,b': 6}), 'a%2Cb=6')
   })
   it('It sould support multi keys', function () {
-    assert.deepStrictEqual(api._param({page: 1, size: 1}), 'page=1&size=1')
+    assert.deepStrictEqual(api.param({page: 1, size: 1}), 'page=1&size=1')
   })
 })
 
 describe('url combination', function () {
   it('It should support url combination', function () {
-    assert.deepStrictEqual(api._join('foo', 'bar'), 'foo/bar')
+    assert.deepStrictEqual(api.join('foo', 'bar'), 'foo/bar')
   })
   it('It should handle trailing slash', function () {
-    assert.deepStrictEqual(api._join('foo/', 'bar'), 'foo/bar')
+    assert.deepStrictEqual(api.join('foo/', 'bar'), 'foo/bar')
   })
   it('It should handle initial slash', function () {
-    assert.deepStrictEqual(api._join('foo', '/bar'), 'foo/bar')
+    assert.deepStrictEqual(api.join('foo', '/bar'), 'foo/bar')
   })
   it('It should handle both initial and trailing slash', function () {
-    assert.deepStrictEqual(api._join('foo/', '/bar'), 'foo/bar')
+    assert.deepStrictEqual(api.join('foo/', '/bar'), 'foo/bar')
   })
   it('It should support more than two parameters', function () {
-    assert.deepStrictEqual(api._join('foo', 'bar', 'baz'), 'foo/bar/baz')
+    assert.deepStrictEqual(api.join('foo', 'bar', 'baz'), 'foo/bar/baz')
   })
 })
 
